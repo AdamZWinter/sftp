41a155f723e495e7bb6727746edf8c0437cb88e6
Adam Winter <adam@bootstrapmusic.com>
2022-07-16 11:33:45 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,6 +68,7 @@ resource "azurerm_public_ip" "resourceTrackingNamePublicIPone" {
 
 resource "azurerm_lb" "resourceTrackingNameTestLB" {
   name                = "sftptestloadbalancer"
+  sku                 = "Standard"
   resource_group_name = azurerm_resource_group.resourceTrackingNameRG.name
   location            = azurerm_resource_group.resourceTrackingNameRG.location
 
