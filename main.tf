4cead5cdaa953fce4dcbcf7805751e1c36bf557b
Adam Winter <adam@bootstrapmusic.com>
2022-07-15 17:08:16 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,35 +8,7 @@ terraform {
   }
 }
 
-variable "SP_CLIENT_ID" {
-  description = "Azure Client ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "SP_CLIENT_SECRET" {
-  description = "Azure Client Service Principal Secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "SUBSCRIPTION_ID" {
-  description = "Azure Subscription ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "SP_TENANT_ID" {
-  description = "Azure Tenant ID"
-  type        = string
-  sensitive   = true
-}
-
 provider "azurerm" {
-  subscription_id = var.SUBSCRIPTION_ID
-  client_id       = var.SP_CLIENT_ID
-  client_secret   = var.SP_CLIENT_SECRET
-  tenant_id       = var.SP_TENANT_ID
   features {}
 }
 
