891b5bbfbf2a896269a1c332461b90db76e6d768
Adam Winter <adam@bootstrapmusic.com>
2022-07-15 18:37:33 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "resourceNameTFscopeOnly" {
+resource "azurerm_resource_group" "resourceTrackingNameRG" {
   name     = "myTFResourceGroup"
   location = "westus2"
 }
